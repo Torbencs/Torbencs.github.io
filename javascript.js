616ea63d46e9332e5581327aa3e46f87537dec18
Torben <tschlawe@gmail.com>
2020-01-28 09:30:40 +1100

    tryagain

--- javascript.js
+++ javascript.js
@@ -26,7 +26,7 @@
       
       .then(({ data: { text } }) => {
       
-        var url = text.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+        var url =  text.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g).toLowerCase();
 
         if (url != '' && url !== null) {
 
