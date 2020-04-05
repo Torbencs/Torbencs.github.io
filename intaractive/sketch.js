3436efb3f7afbbb3978ea09cde024e59fd4073e0
Torben <tschlawe@gmail.com>
2020-04-05 23:26:13 +1000

    gyro debug 2.74

--- intaractive/sketch.js
+++ intaractive/sketch.js
@@ -66,9 +66,9 @@ function draw() {
     document.getElementById('text_1').innerHTML = rotationY;
     document.getElementById('text_2').innerHTML = rotationX;
    
-    findMode(4);
+    //findMode(4);
 
-    document.getElementById('text_3').innerHTML = modeX;
+    document.getElementById('text_3').innerHTML = findMode(rotationY);
     document.getElementById('text_4').innerHTML = modeY;
 
 
@@ -98,11 +98,12 @@ function findMode(rotationData) {
       } else {
         full = true;
         hasRun = true;
-        modeX = mode(tempArray);
+        modeX = mode(tempArray)
+        return modeX;
       }
     }
   } else {
-    return
+    return modeX;
   }
 }
 /*
