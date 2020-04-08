2917a79c35c53d61961aee957c7d55e381686491
Torben <tschlawe@gmail.com>
2020-04-09 01:47:59 +1000

    offset 3.83

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -271,7 +271,7 @@ window.addEventListener('DOMContentLoaded', function(){
         if (axis == 'x') {
             return (Math.cos(57.4433342078932) * rotationDataY) + (Math.sin(237.4433342078932) * rotationDataX) ;
         } else if (axis == 'y') {
-            return (-(Math.sin(57.4433342078932)) * rotationDataY) + (Math.cos(237.4433342078932) * rotationDataX);
+            return (Math.sin(57.4433342078932) * rotationDataY) + (Math.cos(237.4433342078932) * rotationDataX);
         } else {
             console.log("Missing or incorrect axis argument in findOffset function call");
         }
