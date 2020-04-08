c578aab5a40c437bdecbf1ef1f2cbe53e841d590
Torben <tschlawe@gmail.com>
2020-04-09 06:59:06 +1000

    heli roll 4.4

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -143,8 +143,8 @@ window.addEventListener('DOMContentLoaded', function(){
                     document.getElementById('1').innerHTML = calibrateGyroX;
                     document.getElementById('2').innerHTML = calibrateGyroY;
                     
-                    heliMesh.rotation.x = 10 * calibrateGyroX;
-                    heliMesh.rotation.z = 10 * (-calibrateGyroY);
+                    heliMesh.rotation.x = 10 * findOffset( 'x', calibrateGyroX, calibrateGyroY);
+                    heliMesh.rotation.z = 10 * findOffset( 'y', calibrateGyroX, calibrateGyroY);
                     heliMesh.position.x = newPosX;
                     heliMesh.position.z = newPosY; 
                     
