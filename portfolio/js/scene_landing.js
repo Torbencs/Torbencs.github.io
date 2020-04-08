6dcd85e3a9ba0a0a05da867296bf066af598a028
Torben <tschlawe@gmail.com>
2020-04-08 22:16:57 +1000

    axis offset 1.7

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -137,7 +137,7 @@ window.addEventListener('DOMContentLoaded', function(){
                     //Find new coords adjusted for camera offset. Args: theta, axis ( 'x' || 'y'), rotationDataX, rotationDataY
                     
                     //newPosX = positionX + findOffset( 'x', calibrateGyroX, calibrateGyroY);
-                    newPosY = positionY + findOffset( 'y', calibrateGyroX, calibrateGyroY);
+                    newPosY = positionY + findOffset( 'y', calibrateGyroX, calibrateGyroY) + findOffset( 'x', calibrateGyroX, calibrateGyroY);
                    
                     
                     //heliMesh.position.x = newPosX;
