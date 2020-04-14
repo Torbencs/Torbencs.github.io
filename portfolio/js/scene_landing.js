17508a0f1c084778f898fa3a8509b9c7ca3d9701
Torben <tschlawe@gmail.com>
2020-04-15 00:26:59 +1000

    gimbal lock 1.7

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -161,7 +161,7 @@ window.addEventListener('DOMContentLoaded', function(){
                    
                    // heliMesh.rotation.x = 2 * findOffset( 'x', calibrateGyroX, calibrateGyroY);
                     //heliMesh.rotation.z = 2 * -(findOffset( 'y', calibrateGyroX, calibrateGyroY));
-                    heliMesh.position.x = newPosX;
+                    heliMesh.position.x = 0;
                     heliMesh.position.z = newPosY; 
                     
                 }
@@ -292,7 +292,7 @@ window.addEventListener('DOMContentLoaded', function(){
         if (axis == 'x') {
             return (Math.cos(57.4433342078932) * rotationDataY) + (Math.sin(57.4433342078932) * rotationDataX) ;
         } else if (axis == 'y') {
-            return ((Math.sin(57.4433342078932) * rotationDataY)) + (Math.cos(57.4433342078932) * rotationDataX);
+            return ((Math.sin(57.4433342078932) * -rotationDataY)) + (Math.cos(57.4433342078932) * rotationDataX);
         } else {
             console.log("Missing or incorrect axis argument in findOffset function call");
         }
