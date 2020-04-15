ad149b7b278f7f7bb2d4022e13350cf89cad6780
Torben <tschlawe@gmail.com>
2020-04-16 04:57:59 +1000

    gimbal lock day 21 - quaternion conversion V3.3

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -119,8 +119,8 @@ window.addEventListener('DOMContentLoaded', function(){
             heliMesh.position.z = -5;
             heliMesh.position.y = 7;
             heliMesh.enableQuaternions = null;
-            //quat = new BABYLON.Quaternion();
-            //euler = new BABYLON.Vector3.Zero();
+            quat = new BABYLON.Quaternion();
+            euler = new BABYLON.Vector3.Zero();
             
 
             scene.registerBeforeRender( () => {
