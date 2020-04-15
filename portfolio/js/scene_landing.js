c13adb890f6a7d07065ea412849802aae42cb5c1
Torben <tschlawe@gmail.com>
2020-04-15 22:38:09 +1000

    gimbal lock day 21 V1

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -125,8 +125,8 @@ window.addEventListener('DOMContentLoaded', function(){
                     positionX = heliMesh.position.x;
                     positionY = heliMesh.position.z;
 
-                   // if (rotationX < -20){newRotationX = -20} else if (rotationX > 20){newRotationX = 20} else {newRotationX = rotationX};
-                    //if (rotationY < -20){newRotationY = -20} else if (rotationY > 20){newRotationY = 20} else {newRotationY = rotationY};
+                    if (rotationX < -20){newRotationX = -20} else if (rotationX > 20){newRotationX = 20} else {newRotationX = rotationX};
+                    if (rotationY < -20){newRotationY = -20} else if (rotationY > 20){newRotationY = 20} else {newRotationY = rotationY};
                     
                     if (!modeY) {
                         modeX = findMode(rotationX);
@@ -134,8 +134,8 @@ window.addEventListener('DOMContentLoaded', function(){
                     }
                     
                     //Adjust gyro data so zero is natural hand help position and then apply dampening
-                    calibrateGyroX = findCal(modeX, rotationX) * -0.0003; //-0.0008
-                    calibrateGyroY = findCal(modeY, rotationY) * -0.0003;
+                    calibrateGyroX = findCal(modeX, newRotationX) * -0.0003; //-0.0008
+                    calibrateGyroY = findCal(modeY, newRotationY) * -0.0003;
 
                    
                     //Find new coords adjusted for camera offset. Args: axis ( 'x' || 'y'), rotationDataX, rotationDataY
