3b102294cff29d8f5dc28c3ba461fb41d758d7fd
Torben <tschlawe@gmail.com>
2020-04-15 21:42:56 +1000

    position bug fix

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -122,7 +122,7 @@ window.addEventListener('DOMContentLoaded', function(){
                 if (heliMesh && rotationY){
 
                     positionX = heliMesh.position.x;
-                    positionY = heliMesh.position.y;
+                    positionY = heliMesh.position.z;
 
                    // if (rotationX < -20){newRotationX = -20} else if (rotationX > 20){newRotationX = 20} else {newRotationX = rotationX};
                     //if (rotationY < -20){newRotationY = -20} else if (rotationY > 20){newRotationY = 20} else {newRotationY = rotationY};
