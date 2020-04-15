5a80b79eeb34b4124a65a9330e4209b4d6432468
Torben <tschlawe@gmail.com>
2020-04-16 00:49:00 +1000

    gimbal lock day 21 - quaternion conversion V1.6

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -127,10 +127,10 @@ window.addEventListener('DOMContentLoaded', function(){
                     positionX = heliMesh.position.x;
                     positionY = heliMesh.position.z;
                     
-                    quat.toEulerAnglesToRef(euler);
+                    /*quat.toEulerAnglesToRef(euler);
                     setTimeout(()=>{
                         alert(euler);
-                    },3000);
+                    },3000);*/
 
                     if (rotationX < -40){newRotationX = -40} else if (rotationX > 40){newRotationX = 40} else {newRotationX = rotationX};
                     if (rotationY < -40){newRotationY = -40} else if (rotationY > 40){newRotationY = 40} else {newRotationY = rotationY};
