d8794adbe8b36fde831a2f3231223daac5dd8445
Torben <tschlawe@gmail.com>
2020-04-16 20:07:42 +1000

    Gyro Gimbal limit axis V1.5

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -142,8 +142,8 @@ window.addEventListener('DOMContentLoaded', function(){
                     }
                     
                     //Adjust gyro data so zero is natural hand help position and then apply dampening
-                    calibrateGyroX = findCal(modeX, newRotationX) * -0.0008; //-0.0008
-                    calibrateGyroY = findCal(modeY, newRotationY) * -0.0006;
+                    calibrateGyroX = findCal(modeX, newRotationX) * -0.0006; //-0.0008
+                    calibrateGyroY = findCal(modeY, newRotationY) * -0.0008;
 
                    
                     //Find new coords adjusted for camera offset. Args: axis ( 'x' || 'y'), rotationDataX, rotationDataY
