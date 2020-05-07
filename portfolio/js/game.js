e17b3f8353d22eb7db0c139e2fbfcae43e3a3e2c
Torben <tschlawe@gmail.com>
2020-05-08 00:58:29 +1000

    Game

--- portfolio/js/game.js
+++ portfolio/js/game.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     let sizeX = window.innerWidth;
     let sizeY = window.innerHeight;
-    let click = 0
+    let click = 0;
   
   
 
@@ -23,9 +23,9 @@ window.addEventListener('DOMContentLoaded', function(){
         scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
     
             
-        var camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(0,5, 15), scene);
+        var camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(0,20, 50), scene);
         camera.minZ = 0.1;
-        camera.setTarget(new BABYLON.Vector3(0,5,0));
+        camera.setTarget(new BABYLON.Vector3(0,20,0));
         camera.maxZ = 500;      
 
         
@@ -50,12 +50,12 @@ window.addEventListener('DOMContentLoaded', function(){
         light_hemi.intensity = 1.2;
    
         //Light visual helpers
-        var lightSphere1 = BABYLON.Mesh.CreateSphere("sphere", 5, 1, scene);
+        var lightSphere1 = BABYLON.Mesh.CreateSphere("sphere", 16, 3, scene);
         lightSphere1.position = new BABYLON.Vector3(3,0,0);
         lightSphere1.material = new BABYLON.StandardMaterial("light2", scene);
         lightSphere1.material.emissiveColor = new BABYLON.Color3(1, 1, 0);
 
-        var lightSphere2 = BABYLON.Mesh.CreateSphere("sphere", 5, 1, scene);
+        var lightSphere2 = BABYLON.Mesh.CreateSphere("sphere", 16, 3, scene);
         lightSphere2.position = new BABYLON.Vector3(-3,0,0);
         lightSphere2.material = new BABYLON.StandardMaterial("light2", scene);
         lightSphere2.material.emissiveColor = new BABYLON.Color3(1, 1, 0);
@@ -64,39 +64,62 @@ window.addEventListener('DOMContentLoaded', function(){
 
         //Model positioning
        
-       
+      let animation1 = new BABYLON.Animation('player1Animation', 'position.y', 60, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+
+      let keys_anim_1 = [{
+            frame : 0,
+            value : 0
+        },{
+            frame : 160,
+            value : 40
+        }];
+    
+        animation1.setKeys(keys_anim_1);
+        
+        let running_anim1, running_anim2;
+        
+        running_anim1 = scene.beginDirectAnimation(lightSphere1, [animation1],0,160, false, 0.8);
+        running_anim2 = scene.beginDirectAnimation(lightSphere2, [animation1],0,160, false, 0.8);
+        
+        running_anim2.pause();
+        running_anim1.pause();
 
         
+   
         
         scene.onPointerObservable.add((pointerInfo) => {
-            switch (pointerInfo.type) {
-                case BABYLON.PointerEventTypes.POINTERDOWN:
+            if (pointerInfo.type == BABYLON.PointerEventTypes.POINTERDOWN) {
+               
                     
-                        click++;
+                        
+                        running_anim2.pause();
+                 
+                       
+                        running_anim1.pause();
+                    
+                
+                    window.setTimeout(()=>{
+                        click ++;
+
+                        if (click % 2 == 0){
+                            running_anim1.pause();
+                            running_anim2.restart();
+                        } else {
+                            running_anim2.pause();
+                            running_anim1.restart();
+                        }
+                    },500);
+                    
+                                                   
                     
             }
          });
         
-         scene.registerBeforeRender(()=>{
-             if (click == 0 ){
-                 
-             } else if (click % 2 == 0) {
-                 target = left;
-                 window.setTimeout(()=>{
-                    lightSphere1.position.y += 0.03;
-                 },200)
-                 
-             } else {
-                target = right;
-                
-             }
-         })
-      
-       
         return scene;    
             };
 
 
+   
    
     
     //Call the createScene function
@@ -128,51 +151,68 @@ window.addEventListener('DOMContentLoaded', function(){
     
     });
 
+
+let game_control = {
+    
  
 
-    //Timer object
-    Timer = function(time, scene, callback) {
+    "start" : mesh => {
+        let pause = window.setTimeout(()=>{
+            mesh.position.y += 0.05;
+        },2000)
+    },
 
-        this.maxTime = this.currentTime = time;
-        this.isOver = false;
-        this.paused = false;
-        this.started = false;
-        this.callback = callback;
-        this.scene  = scene;
-    
-        var _this = this;
-        scene.registerBeforeRender(function() {
-            if (_this.started && !_this.isOver && !_this.paused) {
-                _this._update();
-            }
-        });
-    };
-    
-    Timer.prototype.reset = function() {
-        this.currentTime = this.maxTime;
-        this.isOver = false;
-        this.started = false;
-    };
-    
-    Timer.prototype.start = function() {
-        this.started = true;
-    };
-    
-    Timer.prototype.pause = function() {
-        this.paused = true;
-    };
-    
-    Timer.prototype.resume = function() {
-        this.paused = false;
-    };
-    
-    Timer.prototype._update = function() {
-        this.currentTime -= this.scene.getEngine().getDeltaTime();
-        if (this.currentTime <= 0) {
-            this.isOver = true;
-            this.callback();
+    /**
+ * @param {string} mesh - Mesh to animate
+ */
+    "stop" : mesh => {
+        mesh.position.y = mesh.position.y;
+    },
+
+}
+
+
+
+Timer = function(time, scene, callback) {
+
+    this.maxTime = this.currentTime = time;
+    this.isOver = false;
+    this.paused = false;
+    this.started = false;
+    this.callback = callback;
+    this.scene  = scene;
+
+    var _this = this;
+    scene.registerBeforeRender(function() {
+        if (_this.started && !_this.isOver && !_this.paused) {
+            _this._update();
         }
-    };
-    
-    
-    
+    });
+};
+
+Timer.prototype.reset = function() {
+    this.currentTime = this.maxTime;
+    this.isOver = false;
+    this.started = false;
+};
+
+Timer.prototype.start = function() {
+    this.started = true;
+};
+
+Timer.prototype.pause = function() {
+    this.paused = true;
+};
+
+Timer.prototype.resume = function() {
+    this.paused = false;
+};
+
+Timer.prototype._update = function() {
+    this.currentTime -= this.scene.getEngine().getDeltaTime();
+    if (this.currentTime <= 0) {
+        this.isOver = true;
+        this.callback();
+    }
+};
+
