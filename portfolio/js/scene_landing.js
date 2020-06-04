00d6b053c77e7b91ae9c29d608b8281f8cbe47f5
Torben <tschlawe@gmail.com>
2020-06-04 21:14:28 +1000

    Lag issue v1

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -1264,7 +1264,7 @@ window.addEventListener('DOMContentLoaded', function(){
     });
     
     //Mobile quality
-    //engine.setHardwareScalingLevel(0.5)
+    engine.setHardwareScalingLevel(0.5)
     
   
     
