1eb702619751d217b58f981d35bccee02d89d6da
Torben <tschlawe@gmail.com>
2020-06-05 04:30:37 +1000

    Btn issue

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -675,35 +675,31 @@ window.addEventListener('DOMContentLoaded', function(){
         let hits = 0;
         let endHits = 0;
     
-        let o;
-        while (!o){
-            if(currentScene == 4){
-                    
-            //Create Start button
-            var button1 = document.createElement("button");
-            button1.style.top = (window.innerHeight / 2) - 30 + "px";
-            button1.style.left = (window.innerWidth / 2) - 75 + "px";
-            button1.textContent = "Start";
-            button1.style.width = "150px";
-            button1.style.height = "60px";
+        // //Create Start button
+        // var button1 = document.createElement("button");
+        // button1.style.top = (window.innerHeight / 2) - 30 + "px";
+        // button1.style.left = (window.innerWidth / 2) - 75 + "px";
+        // button1.textContent = "Start";
+        // button1.style.width = "150px";
+        // button1.style.height = "60px";
+    
+        // button1.setAttribute = ("id", "but1");
+        // button1.classList.add('btn--action');
+        // button1.style.position = "absolute";
+    
+        // document.body.appendChild(button1);
         
-            button1.setAttribute = ("id", "but1");
-            button1.classList.add('btn--action');
-            button1.style.position = "absolute";
-        
-            document.body.appendChild(button1);
-            
-            button1.addEventListener("click", () => {
-                startScene();
-                startRun();
+        // button1.addEventListener("click", () => {
+        //     startScene();
+        //     startRun();
 
-                let skeleton = scene.getSkeletonByName("Armature");
-                snowboarderIdleAnimatable = skeleton.beginAnimation("idle", true, 2);
-                
-                button1.remove();
-            });
-            o = true;
-            }
+        //     let skeleton = scene.getSkeletonByName("Armature");
+        //     snowboarderIdleAnimatable = skeleton.beginAnimation("idle", true, 2);
+            
+        //     button1.remove();
+        // });
+        
+            
         }
        
     
