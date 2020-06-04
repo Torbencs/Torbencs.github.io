61280028f8d659d1fc45418d2babbdbd137149f6
Torben <tschlawe@gmail.com>
2020-06-05 00:02:04 +1000

    Button issue v1.4

--- portfolio/js/scene_landing.js
+++ portfolio/js/scene_landing.js
@@ -656,30 +656,7 @@ window.addEventListener('DOMContentLoaded', function(){
     
             scene.beginDirectAnimation(camera, [animCameraLandingPos,animCameraLandingTarget], 0, 830, false, 0.6, ()=>{
                 currentScene = 4;
-
-            var button1 = document.createElement("button");
-            button1.style.top = (window.innerHeight / 2) - 30 + "px";
-            button1.style.left = (window.innerWidth / 2) - 75 + "px";
-            button1.textContent = "Start";
-            button1.style.width = "150px"
-            button1.style.height = "60px"
-        
-            button1.setAttribute = ("id", "but1");
-            button1.classList.add('btn--action');
-            button1.style.position = "absolute";
-        
-            document.body.appendChild(button1);
-            
-            // button1.addEventListener("click", () => {
-            //     startScene();
-            //     startRun();
-    
-            //     let skeleton = scene.getSkeletonByName("Armature");
-            //     snowboarderIdleAnimatable = skeleton.beginAnimation("idle", true, 2);
-                
-            //     button1.remove();
-            // });
-
+                button1.style.display = 'block';
             });
         
 
@@ -774,7 +751,31 @@ window.addEventListener('DOMContentLoaded', function(){
         endBox.rotation.z = 0;
         endBox.visibility = 0;
         task.loadedMeshes[0].addChild(endBox);
+
+        //Create Start button
+        var button1 = document.createElement("button");
+        button1.style.top = (window.innerHeight / 2) - 30 + "px";
+        button1.style.left = (window.innerWidth / 2) - 75 + "px";
+        button1.textContent = "Start";
+        button1.style.width = "150px";
+        button1.style.height = "60px";
+        button1.style.display = 'none';
     
+        button1.setAttribute = ("id", "but1");
+        button1.classList.add('btn--action');
+        button1.style.position = "absolute";
+    
+        document.body.appendChild(button1);
+        
+        button1.addEventListener("click", () => {
+            startScene();
+            startRun();
+
+            let skeleton = scene.getSkeletonByName("Armature");
+            snowboarderIdleAnimatable = skeleton.beginAnimation("idle", true, 2);
+            
+            button1.remove();
+        });  
 
         
         function startRun(){
